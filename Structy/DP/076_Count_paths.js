bd0e68adb1b4781a113846e5a8f231e91d1955f0
Gerry Bong <ggbong734@gmail.com>
2022-09-11 15:46:01 -0700

    added comments to structy count path and min change

--- Structy/DP/076_Count_paths.js
+++ Structy/DP/076_Count_paths.js
@@ -2,9 +2,9 @@
 // Write a function, countPaths, that takes in a grid as an argument. In the grid, 'X' represents walls and 'O' represents open spaces. You may only move down or to the right and cannot pass through walls. The function should return the number of ways possible to travel from the top-left corner of the grid to the bottom-right corner.
 
 const countPaths = (grid) => {
-  // todo
+  // time complexity is O(r *c) can only visit each cell once
+  // space O(r * c)
   const memo = Array(grid.length).fill(0).map(e => Array(grid[0].length).fill(0));
-  console.log(memo);
   return dfs(0, 0, grid, memo);
 };
 
